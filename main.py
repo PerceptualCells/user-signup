f652ac90508c4c640f358b86113b364def3a70a1
Kevin Platt <onlycraft@gmail.com>
2019-04-14 23:45:02 -0500

    finishing touches

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import cgi
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
-
+"""
 usr_error = ""
 pass_1_error = ""
 pass_2_error = ""
 email_error = ""
+"""
 
 @app.route("/validate", methods=["POST"])
 def validate():
@@ -22,10 +23,10 @@ def validate():
     email = request.form['e_mail']
     #error messages
     usr_error = ""
-    pass_1_error = ""
-    pass_2_error = ""
+    p_error = ""
+    p2_error = ""
     email_error = ""
-    error = ""
+    
     # Validate Username
     if not has_val(usr_name):
         usr_error = "You have left a necessary field blank :}"
@@ -43,7 +44,6 @@ def validate():
     if usr_name == password1:
         usr_error = "your username and password may not be the same"
         p_error= "your username and password may not be the same"
-     
     if not has_val(password1) or not has_val(password2):
         p_error ="You have left a necessary field blank"
 
@@ -53,38 +53,35 @@ def validate():
 
     if not space(password1):
         p_error = "passwords cannot have spaces"
-
+   
     
     if not length(password1):
         p_error = "password length outside of range (3 < password < 20)"
-        
+    
+
     if not password1.isalnum():
         p_error = "you have unaccepable symbols in your password"
+        
 
     # validate username
-    #if has_val(usr_name):
-    #    if e-mail(usr_name):
-
-        
-    """
-    if not usr_error and not pass_1_error and not pass_2_error and not email_error:
-        return render_template("welcome.html", usr_name = usr_name)
+    if not has_val(usr_name):
+        if not email_chk(usr_name):
+            email_error = "this is not a valid email address"
     else:
-        return render_template ("signup.html",
-        user_name = usr_name,
-        e_mail = email,
-        password = "",
-        usr_error = usr_error)
-    """
+        email_error = ""    
 
     
     #TEST RETURN
-    if error=="":
+    if usr_error=="" and p_error == "" and p2_error == "" and email_error == "":
         return render_template("welcome.html",
-         user_name = usr_name)
+        user_name = usr_name)
     else:
         return render_template("signup.html",
-     usr_error = usr_error, p_error = p_error, user_name = usr_name)    
+        usr_error = usr_error,
+        p_error = p_error,
+        user_name = usr_name,
+        e_error = email_error,
+        p2_error = p2_error)    
 
 def has_val(text):
     if text == "" or text.strip() == "":
@@ -105,14 +102,12 @@ def length(text):
     else:
         return False
 
-#def e-mail(text):
-    if space(text):
-
-
-
-
- 
-
+def email_chk(text):
+    if space(text) and length(text):
+        if "@" in text and "." in text:
+            return True
+    else:
+        return False    
 
 @app.route("/signup", methods=["GET", "POST"])
 def index():
