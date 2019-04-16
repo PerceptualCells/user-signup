211b728f386a85ed6235e9f016b2aca00026bde6
Kevin Platt <onlycraft@gmail.com>
2019-04-15 19:43:10 -0500

    this submission has been validated by the TA

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 from flask import Flask, request, redirect, render_template
-import os
 import cgi
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
-"""
-usr_error = ""
-pass_1_error = ""
-pass_2_error = ""
-email_error = ""
-"""
 
 @app.route("/validate", methods=["POST"])
 def validate():
@@ -28,8 +21,11 @@ def validate():
     email_error = ""
     
     # Validate Username
-    if not has_val(usr_name):
-        usr_error = "You have left a necessary field blank :}"
+    if usr_name == password1:
+        usr_error = "your username and password may not be the same"
+        p_error= "your username and password may not be the same"
+
+    
         
     if not length(usr_name):
         usr_error = "Your username is too long or too short"
@@ -40,12 +36,10 @@ def validate():
     if not space(usr_name):
         usr_error = "you have spaces in your username"
 
+    if not has_val(usr_name):
+        usr_error = "The username field cannot be blank :}"
     # Validate Password
-    if usr_name == password1:
-        usr_error = "your username and password may not be the same"
-        p_error= "your username and password may not be the same"
-    if not has_val(password1) or not has_val(password2):
-        p_error ="You have left a necessary field blank"
+    
 
     if password1 != password2:
         p_error = "Your passwords must match"
@@ -54,34 +48,34 @@ def validate():
     if not space(password1):
         p_error = "passwords cannot have spaces"
    
-    
     if not length(password1):
         p_error = "password length outside of range (3 < password < 20)"
     
 
     if not password1.isalnum():
         p_error = "you have unaccepable symbols in your password"
-        
+
+    if not has_val(password1) or not has_val(password2):
+        p_error ="You have left a necessary field blank"    
 
     # validate username
-    if not has_val(usr_name):
-        if not email_chk(usr_name):
+    if has_val(email):
+        if not email_chk(email):
             email_error = "this is not a valid email address"
     else:
         email_error = ""    
 
-    
+
     #TEST RETURN
     if usr_error=="" and p_error == "" and p2_error == "" and email_error == "":
         return render_template("welcome.html",
-        user_name = usr_name)
+            user_name = usr_name)
     else:
-        return render_template("signup.html",
-        usr_error = usr_error,
-        p_error = p_error,
-        user_name = usr_name,
-        e_error = email_error,
-        p2_error = p2_error)    
+        return render_template("signup.html", usr_error = usr_error,
+            p_error = p_error,
+            user_name = usr_name,
+            e_error = email_error,
+            p2_error = p2_error)    
 
 def has_val(text):
     if text == "" or text.strip() == "":
@@ -97,7 +91,7 @@ def space(text):
         return False
 
 def length(text):
-    if len(text) >= 3 or len(text) <= 20:
+    if len(text) >= 3 and len(text) <= 20:
         return True
     else:
         return False
@@ -111,7 +105,7 @@ def email_chk(text):
 
 @app.route("/signup", methods=["GET", "POST"])
 def index():
-    return render_template('signup.html')
+    return render_template("signup.html")
 
 
 app.run()
